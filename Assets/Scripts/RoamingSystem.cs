c49e0ee5dc18e1266dcc3ad05a0788e0bda31ad7
Thazmania1 <martinezpicazoian@gmail.com>
2025-05-31 16:44:40 +0200

    Unrestricted navigation system

--- Assets/Scripts/RoamingSystem.cs
+++ Assets/Scripts/RoamingSystem.cs
@@ -1,8 +1,39 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoamingSystem : MonoBehaviour
 {
     // Holds all rooms.
     [SerializeField] private List<Room> _rooms;
+
+    // References the object's Image component.
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    // This function is called when clicking a minimap room.
+    public void ChangeRoom(string clickedRoomName)
+    {
+        Debug.Log("I go in MF");
+        // Searchs for the corresponding room.
+        Room matchingRoom = null;
+        foreach (Room room in _rooms)
+        {
+            if (room.Name.Equals(clickedRoomName))
+            {
+                Debug.Log("you MF");
+                matchingRoom = room;
+                break;
+            }
+            Debug.Log("Not you MF");
+        }
+
+        Debug.Log("change MF");
+        // Uses the room's sprite.
+        _image.sprite = matchingRoom.Image;
+    }
 }
---
+++ Assets/Scripts/RoomButtonEventSetup.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+// Dynamically sets up the roaming system.
+public class RoomButtonEventSetup : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject roamingSystemHolder = GameObject.Find("CurrentRoom");
+        RoamingSystem roamingSystemReference = roamingSystemHolder.GetComponent<RoamingSystem>();
+        Button buttonComponent = gameObject.GetComponent<Button>();
+        buttonComponent.onClick.AddListener(() => roamingSystemReference.ChangeRoom(transform.parent.name));
+    }
+}
