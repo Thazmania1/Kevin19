6b35d4687456f06acaec2e2908a4e07fe25f2573
Thazmania1 <martinezpicazoian@gmail.com>
2025-06-04 00:39:32 +0200

    Scuffed flush mechanic

--- Assets/Scripts/Survival/Threats/Skibidi Toilet/SkibidiToiletBehaviour.cs
+++ Assets/Scripts/Survival/Threats/Skibidi Toilet/SkibidiToiletBehaviour.cs
@@ -15,15 +15,23 @@ public class SkibidiToiletBehaviour : MonoBehaviour
     // References the [Interactive] folder.
     [SerializeField] private GameObject _interactive;
 
-    // References the manager's audio source.
-    private AudioSource _audioSource;
+    // References the manager's audio sources.
+    private AudioSource _skibidiSong, _flushSound;
 
-    // Tracks if the skibidi toilet is flushed.
+    // References the interactive skibidi toilet object.
+    [SerializeField] private GameObject _interactiveSkibidiToilet;
+
+    // Tracks if the interactive skibidi toilet is flushed.
     private bool _isFlushed = true;
+
+    // References the current floor bathroom the interactive skibidi toilet is in.
+    private string _currentBathroom = "";
 
     private void Start()
     {
-        _audioSource = gameObject.GetComponent<AudioSource>();
+        AudioSource[] audioSources = gameObject.GetComponents<AudioSource>();
+        _skibidiSong = audioSources[0];
+        _flushSound = audioSources[1];
         StartCoroutine(SkibidiRandomizer());
     }
 
@@ -43,22 +51,39 @@ public class SkibidiToiletBehaviour : MonoBehaviour
         _isFlushed = false;
         RectTransform randomFloor = _minimaps.transform.GetChild(Random.Range(0, 3)) as RectTransform;
         GameObject spawnedSkibidiToilet = Instantiate(_skibidiToiletCuePrefab, randomFloor);
+        _currentBathroom = $"{randomFloor.name.Substring(1, randomFloor.name.Length - 2)}Bathroom";
+        Debug.Log(_currentBathroom);
 
         // The player has until the song ends to deal with the skibidi toilet.
-        _audioSource.Play();
-        while (_audioSource.isPlaying)
+        _skibidiSong.Play();
+        while (_skibidiSong.isPlaying && !_isFlushed)
         {
+            NotifyPlayerCurrentRoom(RoamingSystem.CurrentRoom);
             yield return null;
         }
-
         if (!_isFlushed)
         {
             StartCoroutine(GetComponent<SkibidiToiletJumpscareHandler>().Jumpscare());
         }
         else
         {
-            _audioSource.Stop();
+            _skibidiSong.Stop();
             Destroy(spawnedSkibidiToilet);
+            _interactiveSkibidiToilet.SetActive(false);
+            _currentBathroom = "";
+            _flushSound.Play();
         }
+    }
+
+    // Notifies the script of when the player is currently in the same bathroom as skibidi toilet.
+    public void NotifyPlayerCurrentRoom(Room room)
+    {
+        _interactiveSkibidiToilet.SetActive(room.Name.Equals(_currentBathroom));
+    }
+
+    // Called when clicking an interactable skibidi toilet.
+    public void FlushSkibidiToilet()
+    {
+        _isFlushed = true;
     }
 }
